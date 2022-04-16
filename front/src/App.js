187cf11fed1cc6c33a9337d4641ee8bd4ad5e461
beom220 <beom220@gmail.com>
2022-04-17 01:31:04 +0900

    react router

--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,54 @@
 import React, {useState} from "react";
-import {Grid} from "semantic-ui-react";
+import {Button, Divider, Grid, Header, Segment} from "semantic-ui-react";
 import {LoginUser} from "./auth/login";
 import {Register} from "./auth/register";
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+
+import './index.css';
 
 export function App() {
     const [ isLogin, setIsLogin ] = useState(false);
-    // TODO 로그인 유지
+    // TODO #2 로그인 유지
     return (
-        <>
+        <BrowserRouter>
             <Grid style={{height: '100vh'}} verticalAlign='middle' centered columns={2}>
-                {/* #2 -#1 확인용  */}
-                {!isLogin ?
-                    /* #1 로그인여부를 아래 컴포넌트에서 지정하도록 props 전달 */
-                    (<LoginUser onClick={v => setIsLogin(v)}/>) :
-                    (<div>성공</div>)
-                }
-                <Register/>
+                <Routes>
+                    <Route
+                        path="/"
+                        exact
+                        element={<IndexPage/>}
+                    />
+                    <Route
+                        path="/login"
+                        element={<LoginUser onClick={v => setIsLogin(v)}/>}
+                    />
+                    <Route
+                        path="/register"
+                        element={<Register/>}
+                    />
+                </Routes>
             </Grid>
-        </>
+        </BrowserRouter>
     );
+}
+
+function IndexPage(){
+    return(
+        <Grid.Column width={4}>
+            <Header as='h2' attached='top'>
+                Welcome
+            </Header>
+            <Segment>
+                <Link to="/login">
+                    <Button fluid color="teal" type="submit">로그인</Button>
+                </Link>
+                <Divider/>
+                <Link to="/register">
+                    <Button fluid color="violet" type="button">회원가입</Button>
+                </Link>
+            </Segment>
+        </Grid.Column>
+    )
 }
 
 
