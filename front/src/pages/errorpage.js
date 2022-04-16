6e20b2397fca4155b131552b145a0e65748f6e45
beom220 <beom220@gmail.com>
2022-04-17 01:29:01 +0900

    #4 에러페이지 생성

---
+++ front/src/pages/errorpage.js
@@ -0,0 +1,9 @@
+import {Segment} from "semantic-ui-react";
+
+export function ErrorPage(){
+    return(
+        <Segment key="massive" size="massive">
+            <p>Error</p>
+        </Segment>
+    )
+}
