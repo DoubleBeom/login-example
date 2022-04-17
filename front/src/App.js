a63c92de0702de110860a489316a7f995e829277
beom220 <beom220@gmail.com>
2022-04-17 15:24:08 +0900

    #4 에러페이지 연결

--- front/src/App.js
+++ front/src/App.js
@@ -5,26 +5,31 @@ import {Register} from "./auth/register";
 import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
 
 import './index.css';
+import {ErrorPage} from "./pages/errorpage";
 
 export function App() {
     const [ isLogin, setIsLogin ] = useState(false);
     // TODO #2 로그인 유지
     return (
         <BrowserRouter>
-            <Grid style={{height: '100vh'}} verticalAlign='middle' centered columns={2}>
+            <Grid style={{height: '100vh'}} verticalAlign='middle' centered>
                 <Routes>
                     <Route
                         path="/"
                         exact
-                        element={<IndexPage/>}
+                        element={<IndexPage isLogin={isLogin}/>}
                     />
                     <Route
                         path="/login"
-                        element={<LoginUser onClick={v => setIsLogin(v)}/>}
+                        element={<LoginUser onClick={v => setIsLogin(v)} isLogin={isLogin}/>}
                     />
                     <Route
                         path="/register"
-                        element={<Register/>}
+                        element={<Register onClick={v => setIsLogin(v)} isLogin={isLogin}/>}
+                    />
+                    <Route
+                        path="*"
+                        element={<ErrorPage/>}
                     />
                 </Routes>
             </Grid>
@@ -32,20 +37,32 @@ export function App() {
     );
 }
 
-function IndexPage(){
+function IndexPage(props){
     return(
         <Grid.Column width={4}>
             <Header as='h2' attached='top'>
                 Welcome
             </Header>
             <Segment>
-                <Link to="/login">
-                    <Button fluid color="teal" type="submit">로그인</Button>
-                </Link>
-                <Divider/>
-                <Link to="/register">
-                    <Button fluid color="violet" type="button">회원가입</Button>
-                </Link>
+                {props.isLogin ?
+                    (<>
+                        <Segment>
+                            <p>안녕하세요</p>
+                        </Segment>
+                        <Divider/>
+                        <Button type="button" fluid color="red">logout</Button>
+                    </>)
+                    :
+                    (<>
+                        <Link to="/login">
+                            <Button fluid color="teal" type="submit">login</Button>
+                        </Link>
+                        <Divider/>
+                        <Link to="/register">
+                        <Button fluid color="violet" type="button">register</Button>
+                        </Link>
+                    </>)
+                }
             </Segment>
         </Grid.Column>
     )
